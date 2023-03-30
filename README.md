8076572a2074d4091cc9c6165a841431f37bec00
Wojciech Turek <turekwojtek@gmail.com>
2023-03-30 22:16:28 +0200

    Lab 2 - added calories calculator and recipes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # BMICalculator
 
 In app/ directory is added AppTestRecord.webm -> short mobile screen record with BMI Calc test
+
+30.03.2023
+Added Lab2.webm -> new screen record with app tests
---
+++ app/src/main/java/com/example/tipper/StartPageActivity.java
@@ -0,0 +1,51 @@
+package com.example.tipper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Author Wojciech Turek s21611
+ */
+public class StartPageActivity extends AppCompatActivity {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartPageActivity.this, BmiCalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartPageActivity.this, CaloriesCalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartPageActivity.this, RecipesActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
